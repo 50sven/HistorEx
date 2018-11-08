a2f10809676913bd1e015d7c74efa57d911db51b
50sven <sven.mueller92@gmx.de>
2018-11-08 15:35:21 +0100

    added meta data to parsing.py and small adjustments

--- Read_in_XML_Files_Update.py
+++ Read_in_XML_Files_Update.py
@@ -1,83 +1,65 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct 31 14:00:56 2018
-
-@author: Michael
-"""
-
-from bs4 import BeautifulSoup, SoupStrainer
+from bs4 import BeautifulSoup
 import urllib
-import re
-#import time
 import os
 import requests
-import pickle
-#import sys
-#import xml.etree.ElementTree as ET 
-#from xml.dom import minidom
 
 
-
-
-# Extract all links to the books #
 def create_start_soup(http_link):
-
-    html_page = urllib.request.urlopen(http_link) # the start page with the list of all books
+    """Extract all links to the books
+    """
+    html_page = urllib.request.urlopen(http_link)  # the start page with the list of all books
     html_page = html_page.read()
     soup_1 = BeautifulSoup(html_page, "lxml")
-    
+
     return soup_1
 
 
-# Download the books and write them to directory #
-def download_books (soup_of_books, directory_for_downloads):
-    
-    os.chdir(directory_for_downloads) # Select Directory, where Books should be written into #
-    
+def download_books(soup_of_books, path):
+    """Download the books and write them to directory
+    """
+    os.chdir(path)  # Select Directory, where Books should be written into #
+
     list_with_links = []
     i = 0
-    
-    for url in soup_of_books.find_all('a', attrs={'class' : 'aResultsHeader'}):
+
+    for url in soup_of_books.find_all('a', attrs={'class': 'aResultsHeader'}):
         link_snippet = url.get('href')
-        
-        download_link = "http://www.perseus.tufts.edu/hopper/dl"+link_snippet # only works for our use-case
-        doc_name = "book"+ str(i)
-        
-        with open(doc_name+".xml", 'wb') as file:
-            file.write (requests.get(download_link).content)
-            
-    
+
+        download_link = "http://www.perseus.tufts.edu/hopper/dl" + link_snippet  # only works for our use-case
+        doc_name = "book" + str(i)
+
+        with open(doc_name + ".xml", 'wb') as file:
+            file.write(requests.get(download_link).content)
+
         list_with_links.append(download_link)
-        print("Print Book Nr."+str(i))
-        i = i+1
-        
+        print("Print Book Nr." + str(i))
+        i = i + 1
+
     return list_with_links
-    
 
 
-# Extract the names of all books, after the xml-files are downloaded #
 def get_only_book_titles(bsoup):
+    """Extract the names of all books, after the xml-files are downloaded
+    """
     title_names = []
-    for url in bsoup.find_all('a', attrs={'class' : 'aResultsHeader'}):
-        title_names.append(url.text) # the list collects the names of all books
-        
+    for url in bsoup.find_all('a', attrs={'class': 'aResultsHeader'}):
+        title_names.append(url.text)  # the list collects the names of all books
+
     return title_names
 
 
-
 if __name__ == "__main__":
-    
+
     main_soup = create_start_soup("http://www.perseus.tufts.edu/hopper/collection?collection=Perseus:collection:cwar")
-    
+
     # Download the books to directory(adjust to your system) --> Only necessary once!!! (otherwise comment the line out)
-    download_links = download_books(main_soup, "C:/Users/Michael/Documents/KIT/Information_Service_Engineering/Books_new")
+    # download_links = download_books(main_soup, "C:/Users/Michael/Documents/KIT/Information_Service_Engineering/Books_new")
     ##
-    
+
     title_names = get_only_book_titles(main_soup)
-    
+
     # Save und reload the names -------------> Adjust the Paths #
-    #pickle.dump(title_names, open('C:/Users/Michael/Documents/KIT/Information_Service_Engineering/Book_Titles/Book_Titles.pkl', 'wb')) 
-    #title_Names = pickle.load(open('C:/Users/Michael/Documents/KIT/Information_Service_Engineering/Book_Titles/Book_Titles.pkl', 'rb'))   
-    ##    
-    
-    
+    # import pickle
+    # pickle.dump(title_names, open('C:/Users/Michael/Documents/KIT/Information_Service_Engineering/Book_Titles/Book_Titles.pkl', 'wb'))
+    # title_Names = pickle.load(open('C:/Users/Michael/Documents/KIT/Information_Service_Engineering/Book_Titles/Book_Titles.pkl', 'rb'))
+    # #
--- parsing.py
+++ parsing.py
@@ -1,8 +1,74 @@
 from bs4 import BeautifulSoup
+import re
+
+
+def get_meta_data(soup):
+    """Extract meta data from a soup
+    """
+    # title statement contains title and author
+    # sourcDesc often contains publishing date
+    titleStmt = soup.find("titlestmt")
+    sourceDesc = soup.find("sourcedesc")
+    title = ""
+    author = ""
+    date = 0
+
+    # Look for all title snippets and concatenate them
+    for idx, t in enumerate(titleStmt.find_all("title")):
+        if idx == 0:
+            title += t.text
+        else:
+            title += ", " + t.text
+
+    # Look for author(s)
+    for idx, a in enumerate(titleStmt.find_all("author")):
+        if idx == 0:
+            author += a.text
+        else:
+            author += "; " + a.text
+
+    # Look for editor(s)
+    if not author:
+        for idx, e in enumerate(titleStmt.find_all("editor")):
+            if idx == 0:
+                author += e.text
+            else:
+                author += "; " + e.text
+
+    # Look for society name
+    if not author:
+        fileDesc = soup.find("filedesc")
+        try:
+            orgname = fileDesc.find("orgname").text
+            if orgname in title:
+                author = "Medford Historical Society"
+            else:
+                author = "Somerville Historical Society"
+        except:
+            pass
+
+    # Look for publishing date in source description or publishing statement
+    try:
+        date = re.findall(r'\d{4}', sourceDesc.text)[-1]
+    except:
+        pass
+    if not date:
+        publicationStmt = soup.find("publicationstmt")
+        date = re.findall(r'\d{4}', publicationStmt.text)[-1]
+
+    # Adapt results
+    if not title:
+        title = "-"
+    if not author:
+        author = "-"
+    if not date:
+        date = "-"
+
+    return title, author, date
 
 
 def xml_to_text(file, path, return_text=False):
-    """Extract the text body from an .xml file
+    """Extract meta information and text body from an .xml file
 
     Args:
         file (string): path to .xml file
@@ -10,17 +76,28 @@ def xml_to_text(file, path, return_text=False):
         return_text (boolean): True, if text body should be returned
 
     Returns:
-        text body (string): return the extracted text body
+        text (string): return the extracted meta information and text body
     """
     with open(file) as f:
         soup = BeautifulSoup(f, "html.parser")
+
     with open(path, "w") as t:
+
+        # Extract meta data
+        title, author, date = get_meta_data(soup)
+        t.write("[TITLE] " + title + "\n")
+        t.write("[AUTHOR] " + author + "\n")
+        t.write("[DATE] " + date + "\n\n")
+
+        # Go through each chapter and extract the text
         for idx, chapter in enumerate(soup.find_all("div1")):
             try:
-                t.write("[CHAPTER]" + chapter.head.text + "\n\n")
+                t.write("[CHAPTER] " + chapter.head.text + "\n\n")
             except:
-                t.write("[CHAPTER] No Header")
+                t.write("[CHAPTER] No Header" + "\n\n")
             last_p = "last_word_sentence_or_section"
+
+            # Handle line breaks
             for i, p in enumerate(chapter.find_all("p")):
                 text = p.text.strip().replace("\n", " ").replace("  ", " ") + "\n"
                 if last_p == text:
@@ -28,6 +105,7 @@ def xml_to_text(file, path, return_text=False):
                 last_p = text
                 t.write(text)
             t.write("\n")
+
     if return_text:
         with open(path) as t:
             return t.read()
@@ -38,9 +116,13 @@ if __name__ == "__main__":
     import glob
     import numpy as np
 
-    files = glob.glob('data/books/*')
-    random_file = np.random.choice(files)
+    files = glob.glob('data/books_xml/*')
 
-    for file in files:
-        x = xml_to_text(file, "data/test.txt", return_text=True)
-        break
+    # for idx, file in enumerate(files):
+    #     print(f"Processing: {files[idx]}")
+    #     x = xml_to_text(file, "data/test.txt", return_text=False)
+    #     break
+
+    random_file = np.random.choice(files)
+    print("Processing:", random_file)
+    x = xml_to_text(random_file, "data/test.txt", return_text=False)
