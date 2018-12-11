9c53a6ff6b99a7170ccd3ef7219daf4e010721d5
Daniela Kaun <ugebn@student.kit.edu>
2018-12-11 17:57:54 +0100

    get wikidata and check if human

--- Linking/Link_wikidata.py
+++ Linking/Link_wikidata.py
@@ -7,11 +7,28 @@ import datetime
 # Link each author of a book with its wikidata page
 start = time.time()
 
-df = pd.read_csv('../data/1126_books_info.csv')
+def check_human(id):
+    url = 'https://query.wikidata.org/sparql'
+    query = 'PREFIX entity: <http://www.wikidata.org/entity/> ' \
+            'SELECT ?human ' \
+            'WHERE { entity:' + id + ' wdt:P31 ?human. ' \
+                                     '} '
+    response = requests.get(url, params={'format': 'json', 'query': query})
+    data = response.json()
+    if data['results']['bindings']:
+        if 'Q5' in data['results']['bindings'][0]['human']['value']:
+            human = True
+        else:
+            human = False
+    return human
+
+
+
+df = pd.read_csv('../data/1210_books_info.csv')
 index = 0
 url = "https://www.wikidata.org/w/api.php"
 no_identifier = 1
-
+no_author_wiki_id = set()
 for author in df['Author']:
     # add wikidata identifier
     if ';' in author:
@@ -23,22 +40,30 @@ for author in df['Author']:
         'search': author
     }
     r = requests.get(url, params=params)
-    if r.json()['search']:
-        wiki_id = r.json()['search'][0]['id']
-        df.loc[index, 'Author Wiki id'] = wiki_id
-        df.loc[index, 'Author Wikidata Link'] = 'www.wikidata.org/wiki/' + wiki_id
+    response = r.json()
+    if response['search']:
+        wiki_id = response['search'][0]['id']
+        #TODO get description
+        human = check_human(wiki_id)
+        if human:
+            df.loc[index, 'author_wikidata_id'] = wiki_id
+            df.loc[index, 'author_wikidata_link'] = 'www.wikidata.org/wiki/' + wiki_id
         print(f'Wiki identifier: {wiki_id} of author {author} has been successfully retrieved. Index: {index}')
     else:
         print(f'No Identifier for {author} - count {no_identifier}')
         no_identifier += 1
+        no_author_wiki_id.add(author)
+        print(f'No Wiki Id for: {no_author_wiki_id}')
     index += 1
-    time.sleep(31)
+    time.sleep(32)
+
 
 df.to_csv('../data/' + str(datetime.datetime.now().month) + str(datetime.datetime.now().day) + '_books_info_wikiid.csv',
-          index=False)
+          index=False, sep='|')
+with open('../data/' + str(datetime.datetime.now().month) + str(datetime.datetime.now().day) + '_author_set.txt', 'w+')as author_set:
+    author_set.write(str(no_author_wiki_id))
 print(f'In Total {no_identifier} could not been found in Wikidata')
 print(f'Time Tracking: in total it took {time.time()-start}')
-#TODO add set with author who could not be found on wikidata
 
 # ---- request to get id of an author
 # url = "https://www.wikidata.org/w/api.php"
