d091289b93b7b7364ecdd7dd0d09c65ff6818a18
Daniela Kaun <ugebn@student.kit.edu>
2018-12-19 10:49:18 +0100

    wiki_complete

--- Linking/Extracting.py
+++ Linking/Extracting.py
@@ -11,6 +11,7 @@ import numpy as np
 import time
 
 
+
 def get_soup(file):
     soup = ""
     soup = BeautifulSoup(file, "html.parser")
@@ -43,7 +44,8 @@ def extract_loc(f):
     return location_list
 
 
-def write_df(df, df_index, element_list, nb, booktitle, column_list):
+def write_df(df, df_index, element_list, nb, booktitle):#, column_list):
+    #df = pd.concat([df, pd.DataFrame(columns=column_list)])
     df = df.append({'Booknb': nb, 'Title': booktitle}, ignore_index=True)
     for element in element_list:
         existing_element = list(df)
@@ -71,8 +73,8 @@ location_df = pd.DataFrame(columns=['Booknb', 'Title'])
 name_df_columns = []
 locations_df_columns = []
 for idx, file in enumerate(files):
-    # name_df = pd.DataFrame(columns=['Booknb', 'Title'])
-    # location_df = pd.DataFrame(columns=['Booknb', 'Title'])
+    #name_df = pd.DataFrame(columns=['Booknb', 'Title'])
+    #location_df = pd.DataFrame(columns=['Booknb', 'Title'])
     print(f"Processing: {files[idx]}")
     book_number = re.findall("\d+", file)[1]
 
@@ -81,24 +83,27 @@ for idx, file in enumerate(files):
             f = f.read()
             f = f.decode('utf-8', 'ignore').encode('latin-1', 'ignore').decode('utf-8', 'ignore')
         file_soup = get_soup(f)
+
+
         # get title author, date
         title, author, date = parsing.get_meta_data(file_soup)
         # get mentioned names of the book
         names = extract_pers(f)
         if names:
-            name_df = write_df(name_df, idx, names, book_number, title, '')#, name_df_columns)
+            name_df = write_df(name_df, idx, names, book_number, title)#, name_df_columns)
         else:
             name_df = name_df.append({'Booknb': book_number, 'Title': title}, ignore_index=True)
         # get mentioned locations of the book
         locations = extract_loc(f)
         if locations:
-            location_df = write_df(location_df, idx, locations, book_number, title, '')#, name_df_columns)
+            location_df = write_df(location_df, idx, locations, book_number, title)#, name_df_columns)
         else:
             location_df = location_df.append({'Booknb': book_number, 'Title': title}, ignore_index=True)
         #name_df_columns = get_columns(name_df, name_df_columns)
+        #locations_df_columns = get_columns(location_df, locations_df_columns)
         print(f'Name Dataframe Dim: {name_df.shape}')
         print(f'Location Dataframe Dim: {location_df.shape}')
-        name_df.to_csv('../data/' + str(datetime.datetime.now().month) + str(datetime.datetime.now().day) + '_books_names.csv', index=False)
-        location_df.to_csv('../data/' + str(datetime.datetime.now().month) + str(datetime.datetime.now().day) + '_books_locations.csv', index=False)
+name_df.to_csv('../data/' + str(datetime.datetime.now().month) + str(datetime.datetime.now().day) + '_books_names.csv', index=False)
+location_df.to_csv('../data/' + str(datetime.datetime.now().month) + str(datetime.datetime.now().day) + '_books_locations.csv', index=False)
 print(f'Total Time: {time.time() - start}')
 
--- Linking/Link_wikidata.py
+++ Linking/Link_wikidata.py
@@ -23,38 +23,89 @@ def check_human(id):
     return human
 
 
-
 df = pd.read_csv('../data/1210_books_info.csv')
 index = 0
-url = "https://www.wikidata.org/w/api.php"
+url = 'https://query.wikidata.org/sparql'
 no_identifier = 1
 no_author_wiki_id = set()
+
+item_counter = 0
+country_counter = 0
+birth_counter = 0
+death_counter = 0
+birthplace_counter = 0
+gender_counter = 0
+occupation_counter = 0
+
 for author in df['Author']:
     # add wikidata identifier
     if ';' in author:
         author = author.split(';')[0]
-    params = {
-        'action': 'wbsearchentities',
-        'format': 'json',
-        'language': 'en',
-        'search': author
-    }
-    r = requests.get(url, params=params)
+    #  https://query.wikidata.org/#SELECT%20distinct%20%3FitemLabel%20%3FitemDescription%20%3Fcountry%20%0A%3Fdate_birth%20%3Fdate_death%20%3Fbirth_place%20%3Fgender%20%3Foccupation%0AWHERE%20%0A%7B%0A%20%20%3FitemLabel%20%3Flabel%20%27John%20G.%20B.%20Adams%27%40en.%0A%20%20%3FitemLabel%20wdt%3AP31%20wd%3AQ5%20.%0A%20%20OPTIONAL%20%7B%20%3FitemLabel%20wdt%3AP21%20%3Fgender%20.%7D%0A%20%20OPTIONAL%20%7B%20%3FitemLabel%20wdt%3AP27%20%3Fcountry%20.%7D%0A%20%20OPTIONAL%20%7B%20%3FitemLabel%20wdt%3AP569%20%3Fdate_birth%20.%7D%0A%20%20OPTIONAL%20%7B%20%3FitemLabel%20wdt%3AP570%20%3Fdate_death%20.%7D%0A%20%20OPTIONAL%20%7B%20%3FitemLabel%20wdt%3AP19%20%3Fbirth_place%20.%7D%0A%20%20OPTIONAL%20%7B%20%3FitemLabel%20wdt%3AP106%20%3Foccupation%20.%7D%0A%20%20SERVICE%20wikibase%3Alabel%20%7B%0A%09%09bd%3AserviceParam%20wikibase%3Alanguage%20%22en%22%20.%0A%09%7D%0A%20%20%7D
+    #  https://query.wikidata.org/#SELECT%20distinct%20%3Fitem%20%3FitemLabel%20%3FitemDescription%20%3FcountryLabel%20%0A%3Fdate_birth%20%3Fdate_death%20%3Fbirth_placeLabel%20%3FgenderLabel%20%3FoccupationLabel%0AWHERE%20%0A%7B%0A%20%20%3Fitem%20%3Flabel%20%27Elizabeth%20Cary%20Agassiz%27%40en.%0A%20%20%3Fitem%20wdt%3AP31%20wd%3AQ5%20.%0A%20%20OPTIONAL%20%7B%20%3Fitem%20wdt%3AP21%20%3Fgender%20.%7D%0A%20%20OPTIONAL%20%7B%20%3Fitem%20wdt%3AP27%20%3Fcountry%20.%7D%0A%20%20OPTIONAL%20%7B%20%3Fitem%20wdt%3AP569%20%3Fdate_birth%20.%7D%0A%20%20OPTIONAL%20%7B%20%3Fitem%20wdt%3AP570%20%3Fdate_death%20.%7D%0A%20%20OPTIONAL%20%7B%20%3Fitem%20wdt%3AP19%20%3Fbirth_place%20.%7D%0A%20%20OPTIONAL%20%7B%20%3Fitem%20wdt%3AP106%20%3Foccupation%20.%7D%0A%20%20SERVICE%20wikibase%3Alabel%20%7B%0A%09%09bd%3AserviceParam%20wikibase%3Alanguage%20%22en%22%20.%0A%09%7D%0A%20%20%7D
+    sparql_query = 'SELECT distinct ?item ?itemLabel ?itemDescription ?countryLabel ?date_birth ?date_death ' \
+                   '?birth_placeLabel ?genderLabel ?occupationLabel ' \
+                   'WHERE { ' \
+                   '?item ?label \'' + author + '\'@en. ' \
+                   '?item wdt:P31 wd:Q5. ' \
+                    'OPTIONAL { ?item wdt:P21 ?gender .}' \
+                    'OPTIONAL { ?item wdt:P27 ?country .} ' \
+                    'OPTIONAL { ?item wdt:P569 ?date_birth .} ' \
+                    'OPTIONAL { ?item wdt:P570 ?date_death .} ' \
+                    'OPTIONAL { ?item wdt:P19 ?birth_place .} ' \
+                    'OPTIONAL { ?item wdt:P106 ?occupation .}' \
+                   'SERVICE wikibase:label { ' \
+                        'bd:serviceParam wikibase:language "en". ' \
+                    '}' \
+                   '}'
+    r = requests.get(url, params={'format': 'json', 'query': sparql_query})
     response = r.json()
-    if response['search']:
-        wiki_id = response['search'][0]['id']
-        #TODO get description
-        human = check_human(wiki_id)
-        if human:
-            df.loc[index, 'author_wikidata_id'] = wiki_id
-            df.loc[index, 'author_wikidata_link'] = 'www.wikidata.org/wiki/' + wiki_id
-        print(f'Wiki identifier: {wiki_id} of author {author} has been successfully retrieved. Index: {index}')
-    else:
-        print(f'No Identifier for {author} - count {no_identifier}')
-        no_identifier += 1
-        no_author_wiki_id.add(author)
-        print(f'No Wiki Id for: {no_author_wiki_id}')
+    if response['results']['bindings']:
+        occupation = []
+        for key in response['results']['bindings'][0]:
+            if key == 'item':
+                df.loc[index, 'author_wikidata_id'] = response['results']['bindings'][0]['item']['value']
+                item_counter += 1
+            elif key == 'itemLabel':
+                df.loc[index, 'author'] = response['results']['bindings'][0]['itemLabel']['value']
+            elif key == 'itemDescription':
+                df.loc[index, 'author_description'] = response['results']['bindings'][0]['itemDescription']['value']
+            elif key == 'genderLabel':
+                df.loc[index, 'gender'] = response['results']['bindings'][0]['genderLabel']['value']
+                gender_counter += 1
+            elif key == 'countryLabel':
+                df.loc[index, 'origin'] = response['results']['bindings'][0]['countryLabel']['value']
+                country_counter += 1
+            elif key == 'date_birth':
+                df.loc[index, 'date_birth'] = response['results']['bindings'][0]['date_birth']['value']
+                birth_counter += 1
+            elif key == 'birth_placeLabel':
+                df.loc[index, 'author_wikidata_id'] = response['results']['bindings'][0]['birth_placeLabel']['value']
+                birthplace_counter += 1
+            elif key == 'date_death':
+                df.loc[index, 'author_wikidata_id'] = response['results']['bindings'][0]['date_death']['value']
+                death_counter += 1
+
+    len_bindings = len(response['results']['bindings'])
+    counter = 0
+    if len_bindings > 1:
+        for occup_result in response['results']['bindings']:
+            if counter == len_bindings:
+                break
+            if 'occupationLabel' in response['results']['bindings'][counter].keys():
+                occupation.append(response['results']['bindings'][counter]['occupationLabel']['value'])
+                counter +=1
+        df.loc[index, 'occupation'] = occupation
+        occupation_counter += 1
     index += 1
+    print(f'--- Processed author: {author} ; Index: {index}')
+    print(f'{item_counter / index * 100} % items were found on wikidata.')
+    print(f'{gender_counter / index * 100} % gender information were found on wikidata.')
+    print(f'{country_counter / index * 100} % origin information were found on wikidata.')
+    print(f'{birth_counter / index * 100} % birth information were found on wikidata.')
+    print(f'{birthplace_counter / index * 100} % birhtplace information were found on wikidata.')
+    print(f'{death_counter / index * 100} % death information were found on wikidata.')
+    print(f'{occupation_counter / index * 100} % occupation information were found on wikidata.')
     time.sleep(32)
 
 
@@ -64,28 +115,3 @@ with open('../data/' + str(datetime.datetime.now().month) + str(datetime.datetim
     author_set.write(str(no_author_wiki_id))
 print(f'In Total {no_identifier} could not been found in Wikidata')
 print(f'Time Tracking: in total it took {time.time()-start}')
-
-# ---- request to get id of an author
-# url = "https://www.wikidata.org/w/api.php"
-# query = 'Thomas Wentworth Higginson'
-# params = {
-#     'action': 'wbsearchentities',
-#     'format': 'json',
-#     'language': 'en',
-#     'search': query
-# }
-# r = requests.get(url, params= params)
-# id = r.json()['search'][0]['id']
-# print(id)
-
-
-# ---- get page of author using wikidata library
-# client = Client()
-# author = client.get(id)
-# print(author)
-# print(author.description)
-
-# ---- request to  author page
-# url2 = 'https://www.wikidata.org/w/api.php?action=parse&page=Q24871&format=json'
-# response = requests.get(url2)
-# print(response)
