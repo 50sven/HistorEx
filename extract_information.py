7dc150507542d2b114269f890e38d7f1c1762391
50sven <sven.mueller92@gmx.de>
2019-01-10 13:39:57 +0100

    updated information extraction by tag

--- extract_information.py
+++ extract_information.py
@@ -11,20 +11,52 @@ def get_persons(file, num=10):
 
     Args:
         file (string): path to .xml file
+        num (int): number of persons returned
 
     Returns:
-        data (dictionary): return the extracted persons
+        persons_list (list): list of most common persons
+        frequency_list (list): frequency of persons_list
     """
     soup = BeautifulSoup(open(file, "rb"), "html.parser")
 
-    # Look for all tagged person names
-    persons = [p.text for p in soup.find_all("persname") if " " in p.text]
-    counter = Counter(persons)
-    most_frequent_persons = counter.most_common(num)
-    person_list = [p[0] for p in most_frequent_persons][::-1]
-    frequency_list = [p[1] for p in most_frequent_persons][::-1]
+    # Get text and authname of all persons if available
+    persons = [(p.text, p["authname"]) for p in soup.find_all("persname") if "authname" in p.attrs]
 
-    return person_list, frequency_list
+    # Separate authnames and count the most common
+    # (added half of num parameter, due to next step)
+    authnames = [p[1] for p in persons]
+    counter_authnames = Counter(authnames).most_common(2 * num)
+
+    # For each authname list the names appearing in the raw text.
+    # Throw out names without whitespaces and
+    # count the number of whitespaces for the remaining ones.
+    # (more whitespaces -> more likely to get the full name or more information)
+    names_count = [
+        (
+            list(set([(pers[0], pers[0].count(" "))
+                      for pers in persons
+                      if (p[0] == pers[1] and " " in pers[0])])),
+            p[1]
+        )
+        for p in counter_authnames
+    ]
+
+    # Exclude the raw text names including the number of whitesapces
+    # and exclude the frequencies
+    names_count = [n for n in names_count if n[0]]
+    frequency_list = [n[1] for n in names_count]
+
+    # Create a list of persons by sorting by number of whitespaces and length of name
+    persons_list = [n[0] for n in names_count]
+    persons_list = [sorted(p, key=lambda x: (x[1], len(x[0])),
+                           reverse=True) for p in persons_list]
+    persons_list = [p[0][0] for p in persons_list]
+
+    # Finally, slice the most common persons with their frequencies
+    persons_list = persons_list[::-1][-num:]
+    frequency_list = frequency_list[::-1][-num:]
+
+    return persons_list, frequency_list
 
 
 def get_places(file, num=10):
@@ -32,47 +64,67 @@ def get_places(file, num=10):
 
     Args:
         file (string): path to .xml file
+        num (int): number of places returned
 
     Returns:
-        data (dictionary): return the extracted places
+        places_list (list): list of most common places
+        frequency_list (list): frequency of places_list
     """
     soup = BeautifulSoup(open(file, "rb"), "html.parser")
 
-    # Look for all tagged place names
-    places = [p.text for p in soup.find_all("placename")]
-    counter = Counter(places)
-    most_frequent_places = counter.most_common(num)
-    place_list = [p[0] for p in most_frequent_places][::-1]
-    frequency_list = [p[1] for p in most_frequent_places][::-1]
+    # Get text and authname of all places if available
+    places = [(p["reg"], p["authname"]) for p in soup.find_all("placename") if ("authname" in p.attrs and "reg" in p.attrs)]
 
-    return place_list, frequency_list
+    # Separate authnames and count the most common
+    authnames = [p[1] for p in places]
+    counter_authnames = Counter(authnames).most_common(num)
+
+    # For each authname list the reg names appearing in the tag
+    # and exclude frequencies
+    names_count = [
+        (
+            list(set([place[0] for place in places if p[0] == place[1]])),
+            p[1]
+        )
+        for p in counter_authnames
+    ]
+    frequency_list = [n[1] for n in names_count]
+
+    # Create a list of places by sorting by the longest name with capital letter
+    places_list = [n[0] for n in names_count]
+    places_list = [[n for n in p if n] for p in places_list]
+    places_list = [sorted(p, key=lambda x: (len(x), x[0].isupper()),
+                          reverse=True) for p in places_list]
+    places_list = [p[0] for p in places_list]
+    places_list = places_list[::-1]
+    frequency_list = frequency_list[::-1]
+
+    return places_list, frequency_list
 
 
-def get_persons_and_places(file, num_person=10, num_places=10):
+def get_persons_and_places(file, num_persons=10, num_places=10):
     """Extract most frequent persons and places from xml files by tag
+
+    COMBINES get_persons() and get_places()
+    -> DETAILED INFORMATION IN THE DOCSTRINGS OF EACH FUNCTION
 
     Args:
         file (string): path to .xml file
+        num_persons (int): number of persons returned
+        num_places (int): number of places returned
 
     Returns:
-        data (dictionary): return the extracted persons and places
+        persons_list (list): list of most common persons
+        persons_frequency_list (list): frequency of persons_list
+        places_list (list): list of most common places
+        places_frequency_list (list): frequency of places_list
     """
-    soup = BeautifulSoup(open(file, "rb"), "html.parser")
 
-    # Look for all tagged person and place names
-    persons = [p.text for p in soup.find_all("persname") if " " in p.text]
-    counter = Counter(persons)
-    most_frequent_persons = counter.most_common(num_person)
-    person_list = [p[0] for p in most_frequent_persons][::-1]
-    person_frequency_list = [p[1] for p in most_frequent_persons][::-1]
+    persons_list, persons_frequency_list = get_persons(file, num_persons)
+    places_list, places_frequency_list = get_places(file, num_places)
 
-    places = [p.text for p in soup.find_all("placename")]
-    counter = Counter(places)
-    most_frequent_places = counter.most_common(num_places)
-    place_list = [p[0] for p in most_frequent_places][::-1]
-    place_frequency_list = [p[1] for p in most_frequent_places][::-1]
-
-    return person_list, person_frequency_list, place_list, place_frequency_list
+    return (persons_list, persons_frequency_list,
+            places_list, places_frequency_list)
 
 
 if __name__ == "__main__":
@@ -84,20 +136,20 @@ if __name__ == "__main__":
     files = natsort.natsorted(files)
 
     aggregated_data = dict()
-
+    persons = []
     for idx, file in enumerate(files):
         print(f"Processing: {files[idx]}")
-        person_list, person_frequency_list, place_list, place_frequency_list = get_persons_and_places(file)
+        persons_list, persons_frequency_list, places_list, places_frequency_list = get_persons_and_places(file, num_persons=10, num_places=20)
 
         title = BOOK_TITLES[idx]
 
         aggregated_data[title] = {
             "persons": {
-                "names": person_list,
-                "frequency": person_frequency_list
+                "names": persons_list,
+                "frequency": persons_frequency_list
             },
             "places": {
-                "names": place_list,
-                "frequency": place_frequency_list
+                "names": places_list,
+                "frequency": places_frequency_list
             }
         }
